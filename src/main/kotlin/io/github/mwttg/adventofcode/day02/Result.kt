10cc658c35970b534a4a34232733d996ac609bc5
Marco Wittig <Marco-Wittig@gmx.de>
2022-12-02 13:03:02 +0100

    Refactor Day 02
    - Extract win/draw/lose in own enum

---
+++ src/main/kotlin/io/github/mwttg/adventofcode/day02/Result.kt
@@ -0,0 +1,8 @@
+package io.github.mwttg.adventofcode.day02
+
+enum class Result(val points: Int) {
+
+    WIN(6),
+    DRAW(3),
+    LOSE(0)
+}
--- src/main/kotlin/io/github/mwttg/adventofcode/day02/Shape.kt
+++ src/main/kotlin/io/github/mwttg/adventofcode/day02/Shape.kt
@@ -1,8 +1,5 @@
 package io.github.mwttg.adventofcode.day02
 
-private const val DRAW_SCORE = 3
-private const val WIN_SCORE = 6
-private const val LOSE_SCORE = 0
 private const val ROCK_SCORE = 1
 private const val PAPER_SCORE = 2
 private const val SCISSOR_SCORE = 3
@@ -14,15 +11,15 @@ enum class Shape {
     SCISSOR;
 
     fun getScore(other: Shape): Int = when (Pair(this, other)) {
-        Pair(ROCK, ROCK) -> DRAW_SCORE + ROCK_SCORE
-        Pair(ROCK, PAPER) -> LOSE_SCORE + ROCK_SCORE
-        Pair(ROCK, SCISSOR) -> WIN_SCORE + ROCK_SCORE
-        Pair(PAPER, ROCK) -> WIN_SCORE + PAPER_SCORE
-        Pair(PAPER, PAPER) -> DRAW_SCORE + PAPER_SCORE
-        Pair(PAPER, SCISSOR) -> LOSE_SCORE + PAPER_SCORE
-        Pair(SCISSOR, ROCK) -> LOSE_SCORE + SCISSOR_SCORE
-        Pair(SCISSOR, PAPER) -> WIN_SCORE + SCISSOR_SCORE
-        Pair(SCISSOR, SCISSOR) -> DRAW_SCORE + SCISSOR_SCORE
+        Pair(ROCK, ROCK) -> Result.DRAW.points + ROCK_SCORE
+        Pair(ROCK, PAPER) -> Result.LOSE.points + ROCK_SCORE
+        Pair(ROCK, SCISSOR) -> Result.WIN.points + ROCK_SCORE
+        Pair(PAPER, ROCK) -> Result.WIN.points + PAPER_SCORE
+        Pair(PAPER, PAPER) -> Result.DRAW.points + PAPER_SCORE
+        Pair(PAPER, SCISSOR) -> Result.LOSE.points + PAPER_SCORE
+        Pair(SCISSOR, ROCK) -> Result.LOSE.points + SCISSOR_SCORE
+        Pair(SCISSOR, PAPER) -> Result.WIN.points + SCISSOR_SCORE
+        Pair(SCISSOR, SCISSOR) -> Result.DRAW.points + SCISSOR_SCORE
         else -> throw Exception("Not Possible")
     }
 
